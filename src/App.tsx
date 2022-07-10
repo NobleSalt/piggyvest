aed2035aec967c152c9eee44ae8e155602b5a264
Emmanuel Izuchukwu <izuchukwue2@gmail.com>
2022-07-10 20:13:53 +0100

    added security section

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Hero from 'components/Hero';
 import Nav from 'components/Nav';
+import Security from 'components/Security';
 
 function App() {
   return (
     <div className="Home">
       <Nav />
-      <Hero/>
+      <Hero />
+      <Security />
     </div>
   );
 }
