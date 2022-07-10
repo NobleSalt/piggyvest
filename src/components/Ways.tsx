cc271f0150fa371ea5b792984b92f168bb02783b
Emmanuel Izuchukwu <izuchukwue2@gmail.com>
2022-07-10 23:23:40 +0100

    ok

--- src/components/Ways.tsx
+++ src/components/Ways.tsx
@@ -3,8 +3,13 @@ import 'styles/Ways.scss'
 export default function Ways() {
 
    return (
-      <div className="Ways">
-         Ways
+      <div className="max-width Ways">
+         <div className="Ways-left">
+            <h1 className="Ways-left_title">4 ways to build your savings</h1>
+            <p className="Ways-left_body">Earn 5%-15% when you save with any of these PiggyVest plans.</p>
+            <button className="Ways-left_start">start savings</button>
+         </div>
+         <div className="Ways-right"></div>
       </div>
    )
 }
