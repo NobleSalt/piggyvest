34ebe51ef252d881d581370743b30e9e3ccd5825
Emmanuel Izuchukwu <izuchukwue2@gmail.com>
2022-07-11 13:40:49 +0100

    bottom section of footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,10 @@
+import 'styles/Footer.scss'
+
 export function Footer() {
 
    return <footer className="Footer">
-      Footer
+      <div className="Footer-one"></div>
+      <p className="Footer-two">Piggyvest (formerly piggybank.ng) is the leading online savings & investing platform in Nigeria. For over 6 years, our customers have saved and invested billions of Naira that they would normally be tempted to spend.</p>
+      <div className="Footer-three">2016 - 2022 PiggyTech Global Limited - RC 1405222</div>
    </footer>
 }
