16f21af4dad975417dbd56c766de6e69613300a8
Emmanuel Izuchukwu <izuchukwue2@gmail.com>
2022-07-11 20:27:52 +0100

    left saver section done

--- src/components/Saver.tsx
+++ src/components/Saver.tsx
@@ -21,20 +21,7 @@ export function Saver() {
             </span>
          </div>
 
-         <div className="Saver-right">
-            <h2 className="Saver-right_title">3 Million + customers</h2>
-            <p className="Saver-right_description">Since launching in 2016, over 3,000,000 people have used PiggyVest to manage their money better, avoid over-spending and be more accountable.</p>
-            <div className="Saver-right_register">create free account</div>
-            <div className="Saver-right_socials">
-               <div className="Saver-right_socials-social">
-                  <i className="Saver-right_socials-social_icon"><FaApple /></i>
-                  <p className="Saver-right_socials-social_name">Get on iPhone</p>
-               </div>
-               <div className="Saver-right_socials-social">
-                  <img src={gPlay} className="Saver-right_socials-social_icon" />
-                  <p className="Saver-right_socials-social_name">Get on Android</p>
-               </div>
-            </div>
+         <div className="Saver_container-right">
          </div>
       </div>
 
